27f01ad0922f464270f42e8da152731cb2430dc8
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-06 15:58:23 -0700

    Add forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,64 @@
-function toDoList() {
-  this.toDo = [];
-  this.currentId = 0;
+// Business Logic for To-Do List
+function ToDoList() { 
+  this.toDo = []; // sets array
 }
 
-toDoList.prototype.addTask = function(task) {
-  task.id = this.assignId();
-  this.tasks.push(task);
+// Logic for adding Task
+ToDoList.prototype.addTask = function(completeTask) {
+  this.toDo.push(completeTask);
 }
 
-toDoList.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
+// Business Logic for Task
+function Task(item, date) { // constructor, holding all list items
+  this.item = item;
+  this.date = date;
 }
+
+// Business Logic for Task with date
+Task.prototype.completeTask = function(item, date) {
+  return this.item + " by " + this.date;
+}
+
+
+
+// var firstTask = new Task("Cleaning", "12 pm");
+// var secondTask = new Task("Make breakfast", "9am");
+// var thirdTask = new Task("Laundry", "2 pm");
+// console.log(firstTask);
+// console.log(secondTask);
+// console.log(thirdTask);
 
 //front end
 $(document).ready(function() {
-  $("form#form1").submit(function(event) {
+  var taskList = new ToDoList();
+  $("form#form1").submit(function(event) { // adding new task to list
     event.preventDefault();
-    var task = $("input#task").val();
-    $("#output").append("<li>" + task + "</li>");
-  })
+    //  $("#output").append("<li>" + firstTask.item + "</li>")   
+    $("#output").toggle().empty();
+    var newTask = $("#task").val();
+    var newDate = $("#date").val();
+    var firstTask = new Task(newTask, newDate);
 
-  $("#output.children").click(function() {
-    this.addClass("clickedOn");
+    // Task.completeTask(newTask, newDate);
+    taskList.addTask(firstTask);
+    console.log(taskList);
+    // construct task
+
+    // loop through the array of objects
+    // for each object print out the item and date properties
+    taskList.forEach(function(Task) {
+    return (new Task(newTask, newDate))
+    });
+
+  });
+
+  $("ul").on('click','li',function() {
+    console.log($(this));
+    $(this).toggleClass("clickedOn");
+  });
+
+  $("form#removeForm").submit(function(){
+    event.preventDefault();
+    $("#output").empty();
   });
 });
